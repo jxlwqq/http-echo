85695494f79ffa1f68eb0342bd7b366040d3feee
jxlwqq <jxlwqq@gmail.com>
2021-10-29 15:05:59 +0800

    http echo server

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var text = pflag.String("text", "", "text to put on the webpage")
+var addr = pflag.String("addr", ":8080", "address to listen on")
+
+func main() {
+	pflag.Parse()
+
+	if *text == "" {
+		log.Fatal("--text option is required!")
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, *text)
+	})
+
+	srv := http.Server{
+		Addr:    *addr,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+}
