1c04900acecb26003635f763dd399778a1380a83
jxlwqq <jxlwqq@gmail.com>
2021-11-24 19:28:05 +0800

    update log

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+			log.Fatalf("Server listen failed: %s\n", err)
 		}
 	}()
 
@@ -46,7 +46,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown: ", err)
+		log.Fatalf("Server forced to shutdown: %s\n", err)
 	}
 
 	log.Println("Server exiting")
